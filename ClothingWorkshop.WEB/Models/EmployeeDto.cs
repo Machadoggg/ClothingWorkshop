0a545331e8f4e2af1a29f484fde6b7eb4919a29d
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2024-06-19 03:28:09 -0500

    Nulls in model

--- ClothingWorkshop.WEB/Models/EmployeeDto.cs
+++ ClothingWorkshop.WEB/Models/EmployeeDto.cs
@@ -3,8 +3,8 @@
     public class EmployeeDto
     {
         public int EmployeeId { get; set; }
-        public string Name { get; set; }
-        public string Occupation { get; set; }
+        public string Name { get; set; } = default!; 
+        public string Occupation { get; set; } = default!;
         public DateTime HiringDate { get; set; }
     }
 }
